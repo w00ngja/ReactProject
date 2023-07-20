b6d5352cf37b2ac0972eb92295dd9fbf2428060e
Jaewoong Hwang <noa9925@gmail.com>
2023-07-20 19:01:27 +0900

    API (2)
    - axios를 이용한 Mock data, Real data 통신
    - 기본 httpClient 추가적인 params를 붙혀 요청하였음

---
+++ youtube/src/api/youtube.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export default class Youtube {
+  constructor() {
+    // 기본 URL과, 사용되는 Key 설정
+    this.httpClient = axios.create({
+      baseURL: 'https://www.googleapis.com/youtube/v3',
+      params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+    });
+  }
+
+  async search(keyword) {
+    // 호출한 API에서 비디오의 id 값을 불러오는 경로가 다르기 때문에 분리해주었음
+    // 함수 앞에 #을 붙이게 되면 private하게 사용할 수 있음
+    return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
+  }
+
+  async #searchByKeyword(keyword) {
+    // return axios
+    //   .get(`/videos/search.json`)
+    //   .then((res) => res.data.items)
+    //   .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })));
+
+    return this.httpClient
+      .get('search', { params: { part: 'snippet', maxResults: 25, type: 'video', q: keyword } })
+      .then((res) => res.data.items)
+      .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })));
+  }
+
+  async #mostPopular(keyword) {
+    // return axios.get(`/videos/popular.json`).then((res) => res.data.items);
+
+    return this.httpClient
+      .get('vidoes', { params: { part: 'snippet', maxResults: 25, charts: 'mostPopluar' } })
+      .then((res) => res.data.items)
+      .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })));
+  }
+}
--- youtube/src/pages/Videos.jsx
+++ youtube/src/pages/Videos.jsx
@@ -3,17 +3,32 @@ import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard/VideoCard';
 
+import FakeYoutube from '../api/fakeYoutube';
+import Youtube from '../api/youtube';
+
 export default function Videos() {
   const { keyword } = useParams();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(['videos', keyword], async () => {
-    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-      .then((res) => res.json())
-      .then((data) => data.items);
+  } = useQuery(['videos', keyword], () => {
+    // const youtube = new FakeYoutube();
+    const youtube = new Youtube();
+    return youtube.search(keyword);
   });
+  // [1] fetch의 단점
+  // 1. 받아오는 파일을 json 변환해주어야 함
+  // 2. 백엔드 상에서 받아오는 실패한 데이터(404,400 등)도 통신에 성공한 것으로 인식
+  // return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+  // .then((res) => res.json())
+  // .then((data) => data.items);
+
+  // [2] 따라서, Axios 라이브러리를 활용해볼 것
+  // return axios.get(`/videos/${keyword ? 'search' : 'popular'}.json`).then((res) => res.data.items);
+
+  // [3] Mock data와 Real data의 원활한 변환을 위헤, 가독성을 위해 따로 분리한다. (=>youtube.js)
+  // 그래서 useQeury에는 캐싱데이터, 콜백, 캐싱전략만 직관적으로 확인할 수 있게 구성하는 것이 좋다.
 
   return (
     <div className="">
