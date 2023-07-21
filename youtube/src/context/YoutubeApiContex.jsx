0480eccda2cc422717f15bb481ba119a2a218968
Jaewoong Hwang <noa9925@gmail.com>
2023-07-21 15:34:46 +0900

    Update YoutubeApiContex.jsx

--- youtube/src/context/YoutubeApiContex.jsx
+++ youtube/src/context/YoutubeApiContex.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext } from 'react';
 import Youtube from '../api/youtube';
-import FakeYoutubeClient from '../api/fakeYoutubeClient';
+// import FakeYoutubeClient from '../api/fakeYoutubeClient';
 import YoutubeClient from '../api/youtubeClient';
 
 export const YoutubeApiContext = createContext();
